7a30479fc6e0e89174fd78653ec7bfedcd0af15d
Harshaldalvi7797 <[dalviharshal7797@gmail.com]>
2020-03-29 16:19:24 +0530

    commit : scope of variables

---
+++ Scope/main.js
@@ -0,0 +1,29 @@
+let strName = "harshal"; // global variable
+//console.log(intValu1);intValu1 is not defined
+const pi = 3.14;
+{
+  //console.log(intValu1);Cannot access 'intValu1' before initialization
+  let intValu1 = 10; // local variable
+
+  console.log(intValu1);
+  console.log(strName);
+}
+//intValu1 is not defined
+//console.log(intValu1);
+
+//............using var ....//
+console.log(intValue2); //undefined
+{
+  console.log(pi);
+  console.log(intValue2); //undefined
+  var intValue2 = 20; //local variable
+  console.log(intValue2);
+  console.log(strName);
+}
+console.log(intValue2); // it gives 20 value
+
+{
+  let a = 100;
+  var b = 200;
+}
+// console.log(b);
--- variables/main.js
+++ variables/main.js
@@ -1,10 +1,17 @@
-//console.log(intValue);
+//....let..//
+//console.log(intValue); Cannot access 'intValue' before initialization in let
 let intValue = 10;
 
 console.log(intValue);
 
-//console.log(intValue1);
+//  var //
+//console.log(intValue1);before initialization in var it shows undefined
 var intValue1 = 5;
 console.log(intValue1);
-intValue1 = 25;
-console.log(intValue1);
+// intValue1 = 25;
+// console.log(intValue1);
+
+//...const..//
+//console.log(pi);Cannot access 'pi' before initialization
+const pi = 3.14;
+console.log(pi);
