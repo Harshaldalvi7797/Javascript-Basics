203a77f718a9f9f9f8aa3fa70534b9c071a7139e
Harshaldalvi7797 <[dalviharshal7797@gmail.com]>
2020-03-29 15:29:28 +0530

    commit : basic variable in javascript

---
+++ variables/main.js
@@ -0,0 +1,10 @@
+//console.log(intValue);
+let intValue = 10;
+
+console.log(intValue);
+
+//console.log(intValue1);
+var intValue1 = 5;
+console.log(intValue1);
+intValue1 = 25;
+console.log(intValue1);
