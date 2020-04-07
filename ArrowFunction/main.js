49f2be6cf5bb44f7dfe024cbe17a771e06e2651a
Harshaldalvi7797 <[dalviharshal7797@gmail.com]>
2020-04-07 12:22:06 +0530

    commit : can not cal return in single line arrow function

--- ArrowFunction/main.js
+++ ArrowFunction/main.js
@@ -16,10 +16,25 @@ console.log(result);
 
 // Arrow Function
 
-let add3 = () => console.log("Arrow function");
+// let add3 = () => console.log("Arrow function");
+let add3 = () => {
+  console.log("Arrow function");
+  return "hii ";
+};
+// let add3 = () => return "hii"; cannot use return in single line arrow function
 
-add3();
+result = add3();
+console.log(result);
+
 let add4 = strname => {
+  // return "hiiiii";not call
   console.log(strname);
 };
 add4("Harshal");
+
+let add5 = strname => {
+  return strname;
+  console.log(strname);
+};
+result = add5("Harshal");
+console.log(result);
