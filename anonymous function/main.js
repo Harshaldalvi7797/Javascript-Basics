2c014e6f99f5913ddd35aece931de86bd91c8fa9
Harshaldalvi7797 <[dalviharshal7797@gmail.com]>
2020-04-11 14:08:06 +0530

    commit : anonymous function

---
+++ anonymous function/main.js
@@ -0,0 +1,11 @@
+//anonymous function only this 2 ways
+
+(function() {
+  console.log("anonymous function");
+})();
+
+(() => {
+  console.log("arrow");
+})();
+
+//anonymous function not done using function expression
