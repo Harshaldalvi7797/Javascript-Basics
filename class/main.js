2b2b17aae3c903e787d2e2c9c520733ef87f2b69
Harshaldalvi7797 <[dalviharshal7797@gmail.com]>
2020-04-12 16:09:49 +0530

    commit : private method

--- class/main.js
+++ class/main.js
@@ -4,6 +4,11 @@ function users() {
   this.firstName = undefined;
   this.lastName = undefined;
 
+  //private property
+  let firstName = undefined;
+  firstName = "hhh";
+  let lastName = undefined;
+
   //public method
   this.onDisplay = function() {
     console.log(`${this.firstName} ${this.lastName}`);
@@ -13,16 +18,26 @@ function users() {
   let privateMethodDemo = function() {
     console.log("private method");
   };
+
+  this.displayName = function(strName) {
+    firstName = strName;
+    console.log(firstName);
+    privateMethodDemo(); // private method demo
+  };
 }
+
 //create an unstance of user class
 let userObj = new users();
-userObj.firstName = "harshal";
-userObj.lastName = "Dalvi";
+userObj.firstName = "harshal"; // setter property
+userObj.lastName = "Dalvi"; // setter property
 userObj.onDisplay();
+
+userObj.displayName("Saylee");
 
 userObj = null;
 
-userObj.onDisplay(); // once instance is null the it cannot call it once again
+// once instance is null the it cannot call it once again
+// userObj.onDisplay();
 
 // let userObj1 = new users();
 // userObj1.firstName = "harshal";
